594971d4a581707e2468f39520adc87123b6d369
Arnaldo Stanzani <arnaldo.stanzani@gmail.com>
2021-07-24 19:12:36 -0300

    Setup formik for signin

--- app/src/pages/signin/SignIn.tsx
+++ app/src/pages/signin/SignIn.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
   Box,
   Button,
@@ -13,35 +13,26 @@ import { Redirect } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import classNames from 'classnames';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
 import { ReactComponent as SignInSVG } from 'assets/sign_in.svg';
 import { Routes } from 'const';
 import { loginUser } from 'store';
 import { useAuth, useCurrentUser } from 'hooks';
 import useStyles from './styles';
 
+const signInSchema = Yup.object().shape({
+  email: Yup.string().required(),
+  password: Yup.string().required(),
+});
+
 export function SignIn() {
   const dispatch = useDispatch();
   const [, loading, error] = useCurrentUser();
   const isAuthenticated = useAuth();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const { t } = useTranslation();
   const classes = useStyles();
-
-  const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
-  };
-
-  const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
-  };
-
-  const onClickLogin = async (e: React.MouseEvent) => {
-    e.preventDefault();
-    e.stopPropagation();
-
-    dispatch(loginUser({ email, password }));
-  };
 
   const alertClassName = classNames(classes.errorAlert, {
     [classes.errorAlertVisible]: !!error,
@@ -53,77 +44,101 @@ export function SignIn() {
 
   return (
     <Box display="flex" flex="1">
-      <Box
-        component="form"
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        justifyContent="center"
-        flex="1"
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        validationSchema={signInSchema}
+        onSubmit={(values) => {
+          dispatch(
+            loginUser({ email: values.email, password: values.password })
+          );
+        }}
+        validateOnMount
       >
-        <Typography variant="h4" component="h1" className={classes.title}>
-          {t('signIn.welcome')}
-        </Typography>
-        <Typography
-          variant="h5"
-          component="h2"
-          className={classes.title}
-          color="textSecondary"
-        >
-          {t('signIn.signInToContinue')}
-        </Typography>
-        <TextField
-          id="email"
-          className={classes.formInput}
-          variant="outlined"
-          label={t('signIn.form.email')}
-          type="email"
-          onChange={onChangeEmail}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <AlternateEmailOutlined fontSize="small" />
-              </InputAdornment>
-            ),
-          }}
-          placeholder={t('signIn.form.email')}
-        />
-        <TextField
-          id="password"
-          className={classes.formInput}
-          variant="outlined"
-          label={t('signIn.form.password')}
-          type="password"
-          onChange={onChangePassword}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <LockOpen fontSize="small" />
-              </InputAdornment>
-            ),
-          }}
-          placeholder={t('signIn.form.password')}
-        />
-        <Button
-          variant="contained"
-          color="primary"
-          size="large"
-          onClick={onClickLogin}
-        >
-          {loading ? (
-            <CircularProgress
-              color="secondary"
-              size={32}
-              className={classes.spinner}
+        {({ handleChange, handleSubmit, isValid }) => (
+          <Box
+            component="form"
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+            flex="1"
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSubmit();
+            }}
+          >
+            <Typography variant="h4" component="h1" className={classes.title}>
+              {t('signIn.welcome')}
+            </Typography>
+            <Typography
+              variant="h5"
+              component="h2"
+              className={classes.title}
+              color="textSecondary"
+            >
+              {t('signIn.signInToContinue')}
+            </Typography>
+            <TextField
+              id="email"
+              name="email"
+              className={classes.formInput}
+              variant="outlined"
+              label={t('signIn.form.email')}
+              type="email"
+              onChange={handleChange}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <AlternateEmailOutlined fontSize="small" />
+                  </InputAdornment>
+                ),
+              }}
+              placeholder={t('signIn.form.email')}
             />
-          ) : (
-            t('signIn.form.signIn')
-          )}
-        </Button>
-        <Alert variant="outlined" severity="error" className={alertClassName}>
-          {error}
-        </Alert>
-      </Box>
+            <TextField
+              id="password"
+              name="password"
+              className={classes.formInput}
+              variant="outlined"
+              label={t('signIn.form.password')}
+              type="password"
+              onChange={handleChange}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <LockOpen fontSize="small" />
+                  </InputAdornment>
+                ),
+              }}
+              placeholder={t('signIn.form.password')}
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              size="large"
+              disabled={!isValid}
+              type="submit"
+            >
+              {loading ? (
+                <CircularProgress
+                  color="secondary"
+                  size={32}
+                  className={classes.spinner}
+                />
+              ) : (
+                t('signIn.form.signIn')
+              )}
+            </Button>
+            <Alert
+              variant="outlined"
+              severity="error"
+              className={alertClassName}
+            >
+              {error}
+            </Alert>
+          </Box>
+        )}
+      </Formik>
       <Box display="flex" alignItems="center" flex="1">
         <SignInSVG title={''} />
       </Box>
